be3befbd6fd7cbfc08f7931d75b188f4fc28fd77
Quintin Russell <quinn.j.russell@gmail.com>
2021-04-03 13:16:08 -0700

    modified JS data model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -3,60 +3,82 @@ var orderHistory = [
     orderNumber: '11439416898772232',
     orderDate: '08/04/2020',
     currency: 'dollars',
-    listedPrice: 31.55,
     totalPrice: 34.00,
-    productType: 'book',
-    productName: 'JavaScript for impatient programmers',
-    productMaker: 'Rauschmayer, Dr. Axel',
     shipTo: 'JS Masher',
     deliveryStatus: 'Delivered',
     deliveryDate: '08/08/2020',
-    returnable: true,
-    endOfReturnWindow: '09/07/2020'
+    items: [
+      {
+        productType: 'book',
+        name: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        listedPrice: 31.55,
+        returnable: true,
+        endOfReturnWindow: '09/07/2020'
+      }]
   },
   {
     orderNumber: '13456845136215618',
     orderDate: '07/19/2020',
     currency: 'dollars',
-    listedPrice: 41.33,
     totalPrice: 44.53,
-    productType: 'book',
-    productName: 'The Timeless Way of Building',
-    productMaker: 'Alexander, Christopher',
     shipTo: 'JS Masher',
     deliveryStatus: 'Handed directly to resident',
     deliveryDate: '07/20/2020',
-    returnable: true,
-    endOfReturnWindow: '08/19/2020'
+    items: [
+      {
+        productType: 'book',
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        listedPrice: 41.33,
+        returnable: true,
+        endOfReturnWindow: '08/19/2020'
+      }
+    ]
   },
   {
     orderNumber: '11439416898775641',
     orderDate: '07/04/2020',
     currency: 'dollars',
-    listedPrice: 15.98,
     totalPrice: 17.22,
-    productType: 'gaming accessory',
-    productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube adapter',
-    productMaker: 'Nintendo',
     shipTo: 'JS Masher',
     deliveryStatus: 'Handed directly to resident',
     deliveryDate: '07/07/2020',
-    returnable: true,
-    endOfReturnWindow: '08/05/2020'
+    items: [
+      {
+        productType: 'gaming accessory',
+        productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube adapter',
+        productMaker: 'Nintendo',
+        listedPrice: 15.98,
+        returnable: true,
+        endOfReturnWindow: '08/05/2020'
+      }
+    ]
   },
   {
     orderNumber: '11439416898778888',
     orderDate: '07/03/2020',
     currency: 'dollars',
-    listedPrice: [94.95, 33.99],
     totalPrice: 138.93,
-    productType: ['gaming accessory', 'book'],
-    productName: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'JavaScript for impatient programmers'],
-    productMaker: ['Nintendo', 'Faroult, Stephane'],
     shipTo: 'JS Masher',
     deliveryStatus: 'Delivered',
     deliveryDate: '07/05/2020',
-    returnable: [true, true],
-    endOfReturnWindow: ['08/04/2020', '08/04/2020']
+    items: [
+      {
+        productType: 'gaming accessory',
+        productName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        productMaker: 'Nintendo',
+        listedPrice: 94.95,
+        returnable: true,
+        endOfReturnWindow: '08/04/2020'
+      }, {
+        productType: 'book',
+        name: 'JavaScript for impatient programmers',
+        author: 'Faroult, Stephane',
+        listedPrice: 33.99,
+        returnable: true,
+        endOfReturnWindow: '08/04/2020'
+      }
+    ]
   }
 ];
