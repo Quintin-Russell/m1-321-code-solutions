6298ff3d126b337ab8cb5e75339fbc13fc401b15
Quintin Russell <quinn.j.russell@gmail.com>
2021-04-13 21:47:35 -0700

    working

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -8,7 +8,11 @@ function chunk(array, size) {
     for (let i = 0; i < len; i += size) {
       var newArray = [];
       for (let L = i; L < (i + size); L++) {
-        newArray.push(array[L]);
+        if ((array[L] === undefined) || (array[L] === null)) {
+          continue;
+        } else {
+          newArray.push(array[L]);
+        }
       }
       finalArray.push(newArray);
     }
