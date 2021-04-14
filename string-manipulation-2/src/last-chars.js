02673d6aa8c587ab7808dc1a0c7293cb3cedda82
Quintin Russell <quinn.j.russell@gmail.com>
2021-04-13 22:59:46 -0700

    working

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,14 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  var finalStr = '';
+  var len = string.length;
+  if (length > len) {
+    finalStr = string;
+  } else {
+    for (let i = (len - length); i < len; i++) {
+      var toAdd = string[i];
+      finalStr += toAdd;
+    }
+  }
+  return finalStr;
+}
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,13 @@
 /* exported numVowels */
+function numVowels(string) {
+  // for loop if char = aeiou, add to VowelArray the count vowel array
+  var vowels = 0;
+  var char;
+  for (char of string) {
+    char = char.toLowerCase();
+    if ((char === 'a') || (char === 'e') || (char === 'i') || (char === 'o') || (char === 'u')) {
+      vowels = vowels + 1;
+    }
+  }
+  return vowels;
+}
