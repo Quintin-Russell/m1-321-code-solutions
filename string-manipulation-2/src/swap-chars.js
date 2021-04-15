056108c661d806a537de430073f4e23b200f23d8
Quintin Russell <quinn.j.russell@gmail.com>
2021-04-14 21:00:33 -0700

    completed str-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,18 @@
 /* exported swapChars */
+function swapChars(firstIndex, secondIndex, string) {
+  // split string into sub-strings (.split())
+  var strArray = string.split('');
+  // save char @ 1st/2nd index to variable
+  var first = strArray[firstIndex];
+  var second = strArray[secondIndex];
+  // reassign values in array
+  strArray[secondIndex] = first;
+  strArray[firstIndex] = second;
+  // concat array --> str
+  var char;
+  var finalStr = '';
+  for (char of strArray) {
+    finalStr += char;
+  }
+  return finalStr;
+}
