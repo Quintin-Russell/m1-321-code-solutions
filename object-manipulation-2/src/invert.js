3d318b2e4b67e49bd2f7678ee67b1ab1fc4980a3
Quintin Russell <quinn.j.russell@gmail.com>
2021-04-15 18:51:12 -0700

    completed obj-manip-2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,11 @@
 /* exported invert */
+function invert(source) {
+  // for (in) loop: assigns key and value to variables --> assigns value:key to new obj
+  var newObj = {};
+  var key;
+  for (key in source) {
+    var val = source[key];
+    newObj[val] = key;
+  }
+  return newObj;
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,15 +1,22 @@
 /* exported omit */
 function omit(source, keys) {
+  var objKeys = Object.keys(source);
+  objKeys = objKeys.sort();
+  var dupObjKeys = objKeys;
+  dupObjKeys = dupObjKeys.sort();
+  keys = keys.sort();
   var key;
-  // console.log("hi")
+  var newKey;
   for (key of keys) {
-    // console.log("key", key)
-    if (key in source) {
-      delete source[key];
-      // console.log("deleted")
-      // console.log("deleted item",source)
+    var check = objKeys.indexOf(key);
+    if (check !== -1) {
+      var index = objKeys.indexOf(key);
+      dupObjKeys.splice(index, 1);
     }
   }
-  // console.log("source", source)
-  return source;
+  var newObj = {};
+  for (newKey of dupObjKeys) {
+    newObj[newKey] = source[newKey];
+  }
+  return newObj;
 }
