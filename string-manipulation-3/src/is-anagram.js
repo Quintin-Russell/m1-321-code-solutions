734f570560098ca60a62881f43437545021d2db4
Quintin Russell <quinn.j.russell@gmail.com>
2021-04-29 19:04:00 -0700

    working

---
+++ string-manipulation-3/src/is-anagram.js
@@ -0,0 +1,7 @@
+/* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  // remove spaces from strings
+  firstString = firstString.replaceAll(' ', '');
+  secondString = secondString.replaceAll(' ', '');
+  // strings to arrays --> order arrays -- compare index-for-index
+}
