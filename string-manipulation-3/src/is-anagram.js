a4ee7f9a75bb4f4a1725024f29699fa8067dc9ab
Quintin Russell <quinn.j.russell@gmail.com>
2021-04-29 21:51:37 -0700

    working

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,7 +1,21 @@
 /* exported isAnagram */
 function isAnagram(firstString, secondString) {
+  var isAna = true;
   // remove spaces from strings
   firstString = firstString.replaceAll(' ', '');
   secondString = secondString.replaceAll(' ', '');
   // strings to arrays --> order arrays -- compare index-for-index
+  var firstArray = firstString.split('');
+  var secondArray = secondString.split('');
+  firstArray.sort();
+  secondArray.sort();
+  for (var i = 0; i < firstString.length; i++) {
+    if (firstArray[i] === secondArray[i]) {
+      continue;
+    } else {
+      isAna = false;
+      break;
+    }
+  }
+  return isAna;
 }
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,19 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var wordArray = [];
+  // split strings at " "
+  string = string.split(' ');
+  // for loop (outer array); for loop (inner array): creates a new array --> .shift() to reverse order
+  var word;
+  for (word of string) {
+    var array = [];
+    for (var i = 0; i < word.length; i++) {
+      array.unshift(word[i]);
+      // console.log("letter", word[i])
+    }
+    // console.log("array", array)
+    wordArray.push(array);
+  }
+  // console.log(wordArray)
+  // itterate thru word array and add letters to blank str
+}
